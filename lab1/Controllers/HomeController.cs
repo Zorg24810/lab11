20062de1f77c1ad630009f36caa3e11815d67b3b
Zorg24810 <gorb110@tut.by>
2020-08-24 15:18:52 +0300

    Laba2

--- lab1/Controllers/HomeController.cs
+++ lab1/Controllers/HomeController.cs
@@ -1,37 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using lab1.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace lab1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
+            ViewData["Text"] = "Лабораторная работа 2";
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
             return View();
         }
-
-        public IActionResult Privacy()
+        private List<ListDemo> _listDemo;
+        public HomeController()
         {
-            return View();
+            _listDemo = new List<ListDemo>
+            {
+                new ListDemo{ ListItemValue=1, ListItemText="Элемент 1"},
+                new ListDemo{ ListItemValue=2, ListItemText="Элемент 2"},
+                new ListDemo{ ListItemValue=3, ListItemText="Элемент 3"},
+                new ListDemo{ ListItemValue=4, ListItemText="Элемент 4"}
+             };
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+    }  
+
 }
---
+++ lab1/Models/ListDemo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab1.Models
+{
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+    }
+}
